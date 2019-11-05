69d08f490d2f862a40e98fa07bbd25a7741588dd
Pawel Cieszko <pcieszko@soldevelo.com>
2019-11-05 18:23:40 +0100

    OSMS-28: Fixed reducer for sms page

--- owa/app/js/components/SendSms.jsx
+++ owa/app/js/components/SendSms.jsx
@@ -201,7 +201,7 @@ export class SendSms extends React.Component {
   }
 }
 
-const mapStateToProps = state => (state.sendReducer);
+const mapStateToProps = ({ send }) => (send);
 
 const mapDispatchToProps = {
   reset,
