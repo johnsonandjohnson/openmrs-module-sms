b77467cdc034f252d6aef5c29bd11903481be414
Arkadiusz Lalo <alalo@soldevelo.com>
2019-11-14 14:24:25 +0000

    CFLM-193: Changed the approach for adding privileges
    Approved-by: Kamil Madej <kmadej@soldevelo.com>

--- api/src/main/java/org/openmrs/module/sms/api/util/PrivilegeConstants.java
+++ api/src/main/java/org/openmrs/module/sms/api/util/PrivilegeConstants.java
@@ -1,15 +1,10 @@
 package org.openmrs.module.sms.api.util;
 
-import org.openmrs.annotation.AddOnStartup;
-import org.openmrs.annotation.HasAddOnStartupPrivileges;
-
 /**
  * Contains module's privilege constants.
  */
-@HasAddOnStartupPrivileges
 public final class PrivilegeConstants {
 
-	@AddOnStartup(description = "Allows user to manage SMS module")
 	public static final String SMS_MODULE_PRIVILEGE = "SMS module Privilege";
 
 	private PrivilegeConstants() {
